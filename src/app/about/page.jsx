098a1c812d996d1a3a5b194339238895ea9349d5
muhammadderic <muhammad.donny.e@gmail.com>
2024-07-29 15:37:17 +0700

    Add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600 text-center'>About</h1>
+
+      <p className="text-justify">
+        I created this simple web to show movie information.
+      </p>
+
+      <p className="text-justify">
+        Welcome to our movie database website! We are a team of passionate movie enthusiasts who have come together to create a one-stop destination for all your movie-related needs.
+      </p>
+
+      <p className="text-justify">
+        The effort of a developer to create a comprehensive web clone, like a movie information website, involves meticulous planning and execution. It begins with understanding the core functionalities and user experience of the original platform. The developer then moves on to designing an intuitive user interface that mirrors the original while incorporating modern design principles. Integrating a robust and dynamic database to store and retrieve movie information is crucial, requiring proficiency in both front-end and back-end technologies.
+      </p>
+
+      <p>
+        The developer also ensures that the website is responsive, accessible, and optimized for performance across various devices and browsers. Throughout the process, they implement security measures to protect user data and maintain the sites integrity. Rigorous testing and debugging are conducted to identify and fix issues, ensuring a seamless and reliable user experience. This comprehensive approach reflects the developers dedication to creating a high-quality web clone that meets the needs of movie enthusiasts.
+      </p>
+    </div>
+  )
+}
+
+export default About
